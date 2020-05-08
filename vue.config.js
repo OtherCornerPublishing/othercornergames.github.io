0d9ff7c6ac3a40aa3a0762b0226653271a70600f
Darryl Kilzer <darrylkilzer@gmail.com>
2020-05-08 11:51:42 -0600

    change output

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
     // publicPath: process.env.NODE_ENV === 'production'
     //     ? '/othercornergames.github.io/'
     //     : '/',
-    // outputDir: 'docs'
+    outputDir: 'docs'
 }
