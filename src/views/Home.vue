a97b763e647080fa48e825c66fa07e675bb2d986
Darryl Kilzer <darrylkilzer@gmail.com>
2020-05-08 11:43:41 -0600

    push for CI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home container-fluid">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <ColorSchemeExample />
+    <div class="bgs" id="bg-keyframes"></div>
+    <!-- <ColorSchemeExample /> -->
   </div>
 </template>
 
@@ -21,4 +21,84 @@ export default {
 
 
 <style lang="scss" scoped>
+/* all the background scrolling stuff */
+.bgs {
+  z-index: -1; /* put it below the text */
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: url("../assets/backgrounds/forest/ground_3.png") 0 0,
+    url("../assets/backgrounds/forest/ground_2.png") 40% 0,
+    url("../assets/backgrounds/forest/ground_1.png") 60% 0,
+    url("../assets/backgrounds/forest/clouds_2.png") 60% 0,
+    url("../assets/backgrounds/forest/rocks.png") 90% 0,
+    url("../assets/backgrounds/forest/clouds_1.png") 80%, 0,
+    url("../assets/backgrounds/forest/sky.png") 80% 0;
+  -webkit-transition: left 300s linear;
+  -moz-transition: left 300s linear;
+  -o-transition: left 300s linear;
+  transition: left 300s linear;
+}
+
+#bg-hover:hover,
+#bg-target:target {
+  left: -9999px;
+}
+
+#bg-keyframes {
+  -webkit-animation: moving-images 400s linear infinite;
+  -moz-animation: moving-images 400s linear infinite;
+  -o-animation: moving-images 400s linear infinite;
+  animation: moving-images 400s linear infinite;
+}
+
+@keyframes moving-images {
+  0% {
+    left: 0;
+  }
+  50% {
+    left: -9999px;
+  }
+  100% {
+    left: 0;
+  }
+}
+
+@-moz-keyframes moving-images {
+  0% {
+    left: 0;
+  }
+  50% {
+    left: -9999px;
+  }
+  100% {
+    left: 0;
+  }
+}
+
+@-webkit-keyframes moving-images {
+  0% {
+    left: 0;
+  }
+  50% {
+    left: -9999px;
+  }
+  100% {
+    left: 0;
+  }
+}
+
+@-o-keyframes moving-images {
+  0% {
+    left: 0;
+  }
+  50% {
+    left: -9999px;
+  }
+  100% {
+    left: 0;
+  }
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    publicPath: '/themeable-vue-template/',
-    outputDir: 'docs'
+    // publicPath: process.env.NODE_ENV === 'production'
+    //     ? '/othercornergames.github.io/'
+    //     : '/',
+    // outputDir: 'docs'
 }
